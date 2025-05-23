e0b5ca48c844eac43fc37be5d53bafd2e2410c4a
Grégory Sergent <gregorysergentpro@gmail.com>
2025-05-23 21:35:31 +0200

    Adding new project.

--- src/utils/projectsData.ts
+++ src/utils/projectsData.ts
@@ -27,10 +27,20 @@ export const projects: Project[] = [
     title: "Smart Hotel",
     description: "Création d'une application mobile de commande de boissons à l'aide de Qr Code connecté pour un hotel en espagne. Réalisé entièrement en anglais. (Projet BTS 2025-2026)",
     icon: Layout,
-    technologies: ["ReactNative","Node","ReactJS","Expo","Javascript","Docker",],
+    technologies: ["ReactNative","Node","ReactJS","Expo","Javascript","Docker"],
     imageUrl: "https://i.postimg.cc/Z57rw6Mq/image.png",
     status: "En développement",
     githubUrl: "https://github.com/alexwauquier/smart-hotel-mobile-app",
     link: ""
-  }
+  },
+  {
+    title: "FastFeast",
+    description: "Création d'un site web de commande de repas en ligne pour une chaine de fastfood fictif. (Projet personnel)",
+    icon: Layout,
+    technologies: ["HTML","CSS","JavaScript", "APIREST"],
+    imageUrl: "https://postimg.cc/jw3dgt18",
+    status: "Maquette en cours de création",
+    githubUrl: "",
+    link: ""
+  },
 ];
